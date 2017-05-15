1a4af4d440afc8eafb45e3a55b45e732560c2d01
efazlagic1 <efazlagic1@etf.unsa.ba>
2017-05-15 15:02:41 +0200

    Ispravka forme KorisnikRezervacija, dodana forma KorisnikJukebox

--- Projekat/ProjekatMyPub/ProjekatMyPub/KorisnikJukebox/KorisnikJukebox/MainWindow.xaml.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/KorisnikJukebox/KorisnikJukebox/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace prvi
+namespace KorisnikJukebox
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +24,6 @@ namespace prvi
         {
             InitializeComponent();
         }
+
     }
 }
