3095f3c35db06500be71a3090deafffc7ac2c992
tvidovic1 <tvidovic1@etf.unsa.ba>
2017-05-19 17:41:52 +0200

    Prepravka na Korisnik

--- Projekat/ProjekatMyPub/ProjekatMyPub/Model/Korisnik.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Korisnik.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace ProjekatMyPub.Model
 {
+
     abstract class Korisnik
     {
+        private String username;
+        private String password;
+        private String eMail;
+        private Int32 id;
+        private static Int32 zadnjiId = 1;
+
+        public Korisnik(String username, String password, String email)
+        {
+            Username = username;
+            Password = password;
+            EMail = email;
+            Id = zadnjiId;
+            zadnjiId++;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string EMail
+        {
+            get
+            {
+                return eMail;
+            }
+
+            set
+            {
+                eMail = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
     }
+
 }
