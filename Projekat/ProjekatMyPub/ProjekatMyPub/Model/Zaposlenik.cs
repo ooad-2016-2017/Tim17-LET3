b39aa3466487a4b3ee5f2b78eee49ef57269f7af
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-05-19 18:53:17 +0200

    Ispravka klase Zaposlenik

--- Projekat/ProjekatMyPub/ProjekatMyPub/Model/Zaposlenik.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Zaposlenik.cs
@@ -13,7 +13,33 @@ namespace ProjekatMyPub.Model
         private DateTime datumRodjena;
         private String adresa;
         private String telefon;
-        private Decimal 
+        private Decimal baznaPlata = 1000M;
+        private Decimal koeficjent;
+        private Decimal plata;
 
+        public Zaposlenik(String username, String password, String email, String ime, String prezime, DateTime datumRodjenja, String adresa, String telefon, Decimal koeficijent)
+            : base(username, password, email)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjena = datumRodjenja;
+            Adresa = adresa;
+            Telefon = telefon;
+            Koeficjent = koeficjent;
+            Plata = Koeficjent * BaznaPlata;
+
+        }
+
+
+        public String Ime { get => ime; set => ime = value; }
+        public String Prezime { get => prezime; set => prezime = value; }
+        public DateTime DatumRodjena { get => datumRodjena; set => datumRodjena = value; }
+        public String Adresa { get => adresa; set => adresa = value; }
+        public String Telefon { get => telefon; set => telefon = value; }
+        public Decimal BaznaPlata { get => baznaPlata; set => baznaPlata = value; }
+        public Decimal Koeficjent { get => koeficjent; set => koeficjent = value; }
+        public Decimal Plata { get => plata; set => plata = value; }
     }
+
+
 }
