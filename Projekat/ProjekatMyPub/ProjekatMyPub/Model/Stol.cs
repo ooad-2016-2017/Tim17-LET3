ae3338f94899a18a4828730acae15463fa02cccb
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-05-19 19:23:40 +0200

    Dodana klasa Stol

---
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Stol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyPub.Model
+{
+    public class Stol
+    {
+        private Boolean daLiJeZauzet;
+        private Int32 brojStola;
+        private static Int32 zadnjiStol = 1;
+
+        public Stol(Boolean daLiJeZauzet)
+        {
+            DaLiJeZauzet = daLiJeZauzet;
+            BrojStola = zadnjiStol;
+            zadnjiStol++;
+
+        }
+
+        public Boolean DaLiJeZauzet { get => daLiJeZauzet; set => daLiJeZauzet = value; }
+        public Int32 BrojStola { get => brojStola; set => brojStola = value; }
+    }
+}
