397b8481c97c05f29b54dadaa1a021a9767719b0
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-05-19 19:49:39 +0200

    Ispravka

--- Projekat/ProjekatMyPub/ProjekatMyPub/Model/Stol.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Stol.cs
@@ -5,22 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjekatMyPub.Model
-{
-    public class Stol
     {
-        private Boolean daLiJeZauzet;
-        private Int32 brojStola;
-        private static Int32 zadnjiStol = 1;
-
-        public Stol(Boolean daLiJeZauzet)
+        public class Stol
         {
-            DaLiJeZauzet = daLiJeZauzet;
-            BrojStola = zadnjiStol;
-            zadnjiStol++;
+            private Boolean daLiJeZauzet;
+            private Int32 brojStola;
+            private static Int32 zadnjiStol = 1;
 
+            public Stol(Boolean daLiJeZauzet)
+            {
+                DaLiJeZauzet = daLiJeZauzet;
+                BrojStola = zadnjiStol;
+                zadnjiStol++;
+
+            }
+
+            public Boolean DaLiJeZauzet { get => daLiJeZauzet; set => daLiJeZauzet = value; }
+            public Int32 BrojStola { get => brojStola; set => brojStola = value; }
         }
-
-        public Boolean DaLiJeZauzet { get => daLiJeZauzet; set => daLiJeZauzet = value; }
-        public Int32 BrojStola { get => brojStola; set => brojStola = value; }
     }
+}
 }
