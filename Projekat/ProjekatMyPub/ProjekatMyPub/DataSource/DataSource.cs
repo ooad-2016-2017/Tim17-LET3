2d96d022f8ec31857dbdd82b60a705f945fe34ac
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-05-21 18:29:46 +0200

    Dodana klasa Musterija, azuriran DataSource

--- Projekat/ProjekatMyPub/ProjekatMyPub/DataSource/DataSource.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/DataSource/DataSource.cs
@@ -10,12 +10,13 @@ namespace ProjekatMyPub.DataSource
 {
     public class DataSource
     {
-        
+
         private static List<Korisnik> korisnici = new List<Korisnik>()
         {
             new Menadzer("tvidovic1", "password", "tvidovic1@etf.unsa.ba", "Tin", "Vidovic", new DateTime(1996, 8, 6), "Splitska 10", "062/106-016", 2.5M),
             new Zaposlenik("lhasanagic1", "password", "lhasanagic1@etf.unsa.ba", "Lamija", "Hasangic", new DateTime(1996, 4, 9), "Jukiceva 11", "062/410-222", 3M),
-            new Zaposlenik("efazlagic1", "password", "efazlagic1@etf", "Edna", "Fazlagic", new DateTime(1997,7,7), "Midhat Karic - Mitke", "060/321-1841", 3M )
+            new Zaposlenik("efazlagic1", "password", "efazlagic1@etf.unsa.ba", "Edna", "Fazlagic", new DateTime(1997,7,7), "Midhat Karic - Mitke", "060/321-1841", 3M ),
+            new Musterija("kera", "password", "kerah@gmail.com", false)
 
         };
 
---
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Musterija.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyPub.Model
+{
+   public class Musterija : Korisnik
+    {
+        private Boolean vip;
+
+        public Musterija(String username, String password, String email, Boolean vip)
+            : base(username, password, email)
+        {
+            Vip = vip;
+        }
+
+        public Boolean Vip { get => vip; set => vip = value; }
+    
+    }
+}
