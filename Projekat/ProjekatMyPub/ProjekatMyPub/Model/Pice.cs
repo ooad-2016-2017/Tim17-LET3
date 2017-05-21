2d40efd81c7babe1d728e8b838492b0d9772a15d
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-05-21 21:18:41 +0200

    Dodane klase Rezervacija, Pice i StavkaNarudzbe

---
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/Pice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyPub.Model
+{
+    public class Pice
+    {
+        private String naziv;
+        private Decimal cijena;
+        private Int32 kolicina;
+        private Int32 minKolicina;
+        private Int32 kolicinaZaNabavku;
+        private Int32 id;
+        private static Int32 zadnjiId;
+
+        public Pice(String naziv, Decimal cijena, Int32 kolicina, Int32 minKolicina, Int32 kolicinaZaNabavku)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            Kolicina = kolicina;
+            MinKolicina = minKolicina;
+            KolicinaZaNabavku = kolicinaZaNabavku;
+            Id = zadnjiId;
+            zadnjiId++;
+        }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public decimal Cijena { get => cijena; set => cijena = value; }
+        public int Kolicina { get => kolicina; set => kolicina = value; }
+        public int MinKolicina { get => minKolicina; set => minKolicina = value; }
+        public int KolicinaZaNabavku { get => kolicinaZaNabavku; set => kolicinaZaNabavku = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
