24edd48be6be3ca1d3d45b89195e93202ce5cf11
tvidovic1 <tvidovic1@etf.unsa.ba>
2017-06-11 13:45:52 +0200

    Nabavka

---
+++ Projekat/ProjekatMyPub/ProjekatMyPub/Model/MyPubNarucivanje.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyPub.Model
+{
+    public class MyPubNarucivanje
+    {
+        ObservableCollection<Nabavka> stavkeNabavke;
+
+        public ObservableCollection<Nabavka> StavkeNabavke
+        {
+            get
+            {
+                return stavkeNabavke;
+            }
+
+            set
+            {
+                stavkeNabavke = value;
+            }
+        }
+
+        public MyPubNarucivanje()
+        {
+            stavkeNabavke = new ObservableCollection<Nabavka>();
+        }
+
+        public void dodajStavku(Nabavka stavkaNabavke)
+        {
+            StavkeNabavke.Add(stavkaNabavke);
+        }
+    }
+}
