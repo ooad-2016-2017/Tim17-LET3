8da99a3b9c84d589c41bafc54d3a9d4ebac69f83
lhasanagic1 <lhasanagic1@etf.unsa.ba>
2017-06-11 15:53:26 +0200

    Forma Registracija povezana sa LoginVM

--- Projekat/ProjekatMyPub/ProjekatMyPub/View/Registracija.xaml.cs
+++ Projekat/ProjekatMyPub/ProjekatMyPub/View/Registracija.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ProjekatMyPub.Helper;
+using ProjekatMyPub.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,24 @@ namespace ProjekatMyPub.View
         public Registracija()
         {
             this.InitializeComponent();
+
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += ThisPage_BackRequested;
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.DataContext = (LogInVM)e.Parameter;
+        }
+
+        private void ThisPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+
+            INavigationService navigate = new NavigationService();
+            navigate.Navigate(typeof(Login), this.DataContext);
         }
     }
 }
+
